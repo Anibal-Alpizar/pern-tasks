6371a9f17187c5146cae253bc669cd93549f8760
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-04 19:34:30 -0600

    Update routes and error handler in app.js

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,21 @@
 import express from 'express'
 import morgan from 'morgan'
+import tasksRoutes from './routes/tasks.routes.js'
+import authRoutes from './routes/auth.routes.js'
 
 const app = express()
 
+// Middlewares
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => res.json({ message: 'Hello world!' }))
+// Routes
+app.get('/', (req, res) => res.json({ message: "Welcome to my API" }))
+app.use('/api', tasksRoutes)
+app.use('/api', authRoutes)
 
-// Error handler middleware, must be defined last
+// Error handler 
 app.use((err, req, res, next) => {
     res.status(500).json({ status: "error", message: err.message })
 })
