b23fc7ebb395fe8cd55667872469a502daacb022
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-04 19:59:34 -0600

    Add PostgreSQL connection pool configuration

---
+++ server/src/db.js
@@ -0,0 +1,12 @@
+import pg from 'pg';
+
+export const pool = new pg.Pool({
+    port: 5432,
+    host: 'localhost',
+    user: 'postgres',
+    password: '123',
+})
+
+pool.on('connect', () => {
+    console.log('postgres connected');
+})
