19b1543a131efceb78beae1177814b3657403fec
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-05 19:57:48 -0600

    Add error handling to createTask function

--- server/src/controllers/tasks.controller.js
+++ server/src/controllers/tasks.controller.js
@@ -1,8 +1,25 @@
+import { pool } from '../db.js'
+
 export const getAllTasks = (req, res) => res.send('getting tasks')
 
 export const getTask = (req, res) => res.send('getting an task')
 
-export const createTask = (req, res) => res.send('creating a task')
+export const createTask = async (req, res, next) => {
+    const { title, description } = (req.body)
+    try {
+        // throw new Error('force error') // for testing error handling
+        const result = await pool.query(
+            'INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *',
+            [title, description],
+        )
+        res.json(result.rows[0])
+    } catch (error) {
+        if (error.code === '23505') {
+            return res.status(400).json('Task already exists')
+        }
+        next(error) // pass error to error handler
+    }
+}
 
 export const updateTask = (req, res) => res.send('updating a task')
 
