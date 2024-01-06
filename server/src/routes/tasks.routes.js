5a065fd27f6381cd7dac60df52b9d59371808ec3
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-05 21:02:50 -0600

    Update tasks.routes.js to use express-promise-router

--- server/src/routes/tasks.routes.js
+++ server/src/routes/tasks.routes.js
@@ -1,4 +1,4 @@
-import { Router } from "express"
+import Router from "express-promise-router"
 import { createTask, deleteTask, getAllTasks, getTask, updateTask } from "../controllers/tasks.controller.js"
 
 const router = Router()
