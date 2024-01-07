626e14c5282dfaf25d73db444453cc6ba6366899
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-06 19:51:29 -0600

    Add auth middleware for authentication

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken"
+
+export const isAuth = (req, res, next) => {
+    const token = req.cookies.token
+    if (!token) return res.status(401).json({ message: "Unauthorized" })
+
+    jwt.verify(token, 'xyz123',
+        (err, decodedToken) => { // Callback
+            if (err) return res.status(401).json({ message: "Unauthorized" })
+            else req.userId = decodedToken.id
+            next()
+        })
+}
