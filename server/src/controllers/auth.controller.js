f96397c5df5264c6e2e040fee9828e78c92a9039
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-06 19:51:36 -0600

    Add token to cookie in signup endpoint

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -10,7 +10,16 @@ export const signup = async (req, res) => {
         const hashedPassword = await bcrypt.hash(password, 10)
         const result = await pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', [name, email, hashedPassword])
         const token = await createAccessToken({ id: result.rows[0].id })
-        return res.status(201).json({ token })
+
+        // Save token in cookie
+        res.cookie('token', token, {
+            httpOnly: true, // JS cannot access the cookie
+            // secure: true // only works on https
+            sameSite: 'none',
+            maxAge: 24 * 60 * 60 * 1000 // 1 day
+        })
+
+        return res.status(201).json(result.rows[0])
     } catch (error) {
         if (error.code === '23505') {
             return res.status(409).json({ message: 'User already exists' })
