696cc4497dd747d5ab640fb6ad36060caaacf6e0
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-06 20:54:25 -0600

    Add authentication logic to signin endpoint

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -2,7 +2,25 @@ import bcrypt from 'bcrypt'
 import { pool } from '../db.js'
 import { createAccessToken } from '../libs/jwt.js'
 
-export const signin = async (req, res) => res.send('signin')
+export const signin = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const result = await pool.query('SELECT * FROM users WHERE email = $1', [email])
+        if (result.rows.length === 0) return res.status(400).json({ message: 'User not found' })
+        const isValidPassword = await bcrypt.compare(password, result.rows[0].password)
+        if (!isValidPassword) return res.status(400).json({ message: 'Invalid password' })
+        const token = await createAccessToken({ id: result.rows[0].id })
+        res.cookie('token', token, {
+            httpOnly: true, // JS cannot access the cookie
+            // secure: true // only works on https
+            sameSite: 'none',
+            maxAge: 24 * 60 * 60 * 1000 // 1 day
+        })
+        return res.status(200).json(result.rows[0])
+    } catch (error) {
+
+    }
+}
 
 export const signup = async (req, res) => {
     const { name, email, password } = req.body
