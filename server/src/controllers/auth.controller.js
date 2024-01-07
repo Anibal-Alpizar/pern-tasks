115afe772fbb3dcc8057105c412a8122309b5a15
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-06 19:24:28 -0600

    Add JWT token generation for user signup

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcrypt'
 import { pool } from '../db.js'
+import { createAccessToken } from '../libs/jwt.js'
 
 export const signin = async (req, res) => res.send('signin')
 
@@ -7,10 +8,9 @@ export const signup = async (req, res) => {
     const { name, email, password } = req.body
     try {
         const hashedPassword = await bcrypt.hash(password, 10)
-        console.log(hashedPassword)
         const result = await pool.query('INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *', [name, email, hashedPassword])
-        console.log(result)
-        return res.send(result.rows[0])
+        const token = await createAccessToken({ id: result.rows[0].id })
+        return res.status(201).json({ token })
     } catch (error) {
         if (error.code === '23505') {
             return res.status(409).json({ message: 'User already exists' })
