83d314c1cceb2e4ee2d54aa8c58b333bf9c6b2a4
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-08 18:33:48 -0600

    Refactor RegisterPage component

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,7 +1,19 @@
+import { Button, Card, Input } from "../components/ui";
+
 function RegisterPage() {
   return (
-    <div>RegisterPage</div>
-  )
+    <div className="h-[calc(100vh-64px)] flex items-center justify-center">
+      <Card>
+        <h3 className="text-2xl font-bold">Register</h3>
+        <form>
+          <Input placeholder="Enter your full name" />
+          <Input type="email" placeholder="Enter your email" />
+          <Input type="password" placeholder="Enter your password" />
+          <Button>Register</Button>
+        </form>
+      </Card>
+    </div>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
