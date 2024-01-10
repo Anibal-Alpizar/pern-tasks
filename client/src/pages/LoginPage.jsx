8416994aa71dc1effc568e1514887eb33438031d
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-09 22:55:08 -0600

    Add axios post request for user sign in

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,12 +1,16 @@
 import { Card, Input, Button, Label } from "../components/ui";
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
 function LoginPage() {
   const { register, handleSubmit } = useForm();
 
   const onSubmit = handleSubmit(async (data) => {
-    console.log(data);
+    const res = await axios.post(`http://localhost:3000/api/signin`, data, {
+      withCredentials: true,
+    });
+    console.log(res);
   });
 
   return (
