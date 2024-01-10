98b14df69afb9b3e683b771ab5c24043f4a0fa2f
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-09 22:45:50 -0600

    Add login form to LoginPage component

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,7 +1,27 @@
+import { Card, Input, Button, Label } from "../components/ui";
+import { Link } from "react-router-dom";
+
 function LoginPage() {
   return (
-    <div>LoginPage</div>
-  )
+    <div className="h-[calc(100vh-64px)] flex justify-center items-center">
+      <Card>
+        <h1 className="text-4xl font-bold my-2 text-center">Sign in</h1>
+        <form>
+          <Label htmlFor="email">Email</Label>
+          <Input placeholder="Enter your email" />
+          <Label htmlFor="password">Password</Label>
+          <Input placeholder="Enter your password" />
+          <Button>Sign in</Button>
+          <div className="flex justify-between my-4">
+            <p>Dont have an account? </p>
+            <Link to="/register" className="font-bold">
+              Sign up
+            </Link>
+          </div>
+        </form>
+      </Card>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
