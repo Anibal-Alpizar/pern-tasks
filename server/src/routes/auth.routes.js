3848a9a3383cba1e232aad464b0278ad543db757
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-10 16:16:19 -0600

    Add validation middleware to signin and signup routes

--- server/src/routes/auth.routes.js
+++ server/src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import Router from 'express-promise-router';
 import { signin, signup, logout, profile } from '../controllers/auth.controller.js';
 import { isAuth } from '../middlewares/auth.middleware.js';
+import { validateSchema } from '../middlewares/validate.middleware.js';
+import { signinSchema, signupSchema } from '../schemas/auth.schema.js';
 
 const router = Router();
 
-router.post('/signin', signin);
-router.post('/signup', signup);
+router.post('/signin', [validateSchema(signinSchema)], signin);
+router.post('/signup', [validateSchema(signupSchema)], signup);
 router.post('/signout', logout);
 router.get('/profile', [isAuth], profile);
 
