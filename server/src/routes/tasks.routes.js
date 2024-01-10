3c70561818fd229cc51b512117e97341d3a856c0
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-10 16:16:23 -0600

    Update task routes with validation middleware

--- server/src/routes/tasks.routes.js
+++ server/src/routes/tasks.routes.js
@@ -1,13 +1,15 @@
 import Router from "express-promise-router"
 import { createTask, deleteTask, getAllTasks, getTask, updateTask } from "../controllers/tasks.controller.js"
 import { isAuth } from "../middlewares/auth.middleware.js"
+import { validateSchema } from "../middlewares/validate.middleware.js"
+import { createTaskSchema, updateTaskSchema } from "../schemas/task.schema.js"
 
 const router = Router()
 
 router.get('/tasks', [isAuth], getAllTasks)
 router.get('/tasks/:id', [isAuth], getTask)
-router.post('/tasks', [isAuth], createTask)
-router.put('/tasks/:id', [isAuth], updateTask)
+router.post('/tasks', [isAuth, validateSchema(createTaskSchema)], createTask)
+router.put('/tasks/:id', [isAuth, validateSchema(updateTaskSchema)], updateTask)
 router.delete('/tasks/:id', [isAuth], deleteTask)
 
 export default router
