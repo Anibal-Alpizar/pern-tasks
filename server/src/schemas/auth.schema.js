dd3ce8e8ccef29de9a19f31ff1f4204d12c0b358
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-10 19:34:13 -0600

    Update password validation in signupSchema

--- server/src/schemas/auth.schema.js
+++ server/src/schemas/auth.schema.js
@@ -14,7 +14,9 @@ export const signupSchema = z.object({
     password: z.string({
         required_error: 'Password is required',
         invalid_type_error: 'Password must be a string',
-    }).min(6).max(255)
+    }).min(6,{
+        message: 'Password must be at least 6 characters',
+    }).max(255)
 })
 
 
