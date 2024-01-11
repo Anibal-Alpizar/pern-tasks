240a10795193d8128e463da22a7514ea44690969
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-10 19:33:57 -0600

    Refactor signup and signin functions in AuthContext.jsx

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -19,19 +19,34 @@ export function AuthProvider({ children }) {
   const [errors, setErrors] = useState(null);
 
   const signup = async (data) => {
-    const res = await axios.post("http://localhost:3000/api/signup", data, {
-      withCredentials: true,
-    });
-    console.log(res);
-    setUser(res.data);
+    try {
+      const res = await axios.post("http://localhost:3000/api/signup", data, {
+        withCredentials: true,
+      });
+      setUser(res.data);
+      setIsAuth(true);
+      return res.data;
+    } catch (error) {
+      console.log(error);
+      if (Array.isArray(error.response.data))
+        return setErrors(error.response.data);
+      else setErrors([error.response.data.message]);
+    }
   };
 
   const signin = async (data) => {
-    const res = await axios.post(`http://localhost:3000/api/signin`, data, {
-      withCredentials: true,
-    });
-    console.log(res);
-    setUser(res.data);
+    try {
+      const res = await axios.post(`http://localhost:3000/api/signin`, data, {
+        withCredentials: true,
+      });
+      setUser(res.data);
+      setIsAuth(true);
+      return res.data;
+    } catch (err) {
+      console.log(err);
+      if (Array.isArray(err.response.data)) return setErrors(err.response.data);
+      else setErrors([err.response.data.message]);
+    }
   };
 
   return (
