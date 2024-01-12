220e3f9d5b45db1c0dff9c4a60040bb06245f0da
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-11 19:43:26 -0600

    Add navigation links to navbar component

---
+++ client/src/components/navbar/navigation.jsx
@@ -0,0 +1,9 @@
+export const navigation = [
+  { name: "About", path: "/about" },
+  { name: "Login", path: "/login" },
+  { name: "Register", path: "/register" },
+  { name: "Tasks", path: "/tasks" },
+  { name: "NewTask", path: "/tasks/new" },
+  { name: "EditTask", path: "/tasks/1/" },
+  { name: "Profile", path: "/profile" },
+];
