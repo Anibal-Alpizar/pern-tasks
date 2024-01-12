4e641cc1b0aa39007106df2630d5c396820f5fb6
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-11 22:48:22 -0600

    Refactor navigation routes

--- client/src/components/navbar/navigation.jsx
+++ client/src/components/navbar/navigation.jsx
@@ -1,7 +1,10 @@
-export const navigation = [
+export const publicRoutes = [
   { name: "About", path: "/about" },
   { name: "Login", path: "/login" },
   { name: "Register", path: "/register" },
+];
+
+export const privateRoutes = [
   { name: "Tasks", path: "/tasks" },
   { name: "NewTask", path: "/tasks/new" },
   { name: "EditTask", path: "/tasks/1/" },
