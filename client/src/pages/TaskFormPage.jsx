f81589a17fa8f4f1d76142b5a3ca891ec9154346
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-12 22:13:54 -0600

    Add task form functionality

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,7 +1,47 @@
+import { Card, Input, Textarea, Label, Button } from "../components/ui";
+import { useForm } from "react-hook-form";
+import { createTaskRequest } from "../api/tasks.api";
+import { useNavigate } from "react-router-dom";
+
 function TaskFormPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+
+  const onSubmit = handleSubmit(async (data) => {
+    await createTaskRequest(data);
+    navigate("/tasks");
+  });
+
   return (
-    <div>TaskFormPage</div>
-  )
+    <div className="flex h-[80vh] justify-center items-center">
+      <Card>
+        <form onSubmit={onSubmit}>
+          <h2 className="text-3xl font-black my-4">Create Task</h2>
+          <Label htmlFor="title">Task Name</Label>
+          <Input
+            type="text"
+            placeholder="Task Name"
+            autoFocus
+            {...register("title", { required: true })}
+          />
+          {errors.title && (
+            <span className="text-red-500">This field is required</span>
+          )}
+          <Label htmlFor="description">Task Description</Label>
+          <Textarea
+            placeholder="Task Description"
+            rows={3}
+            {...register("description")}
+          ></Textarea>
+          <Button>Create Task</Button>
+        </form>
+      </Card>
+    </div>
+  );
 }
 
-export default TaskFormPage
+export default TaskFormPage;
