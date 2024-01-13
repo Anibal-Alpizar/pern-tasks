f56b3bb9f3be486e89b02e6e74eb43e4dbbba34c
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-12 22:29:03 -0600

    Add task list functionality to TasksPage component

--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+import { getTasksRequest } from "../api/tasks.api";
+import TaskCard from "../components/tasks/TaskCard";
+
 function TaskPage() {
+  const [tasks, setTasks] = useState([]);
+  useEffect(() => {
+    getTasksRequest().then((res) => setTasks(res.data));
+  }, []);
   return (
-    <div>TaskPage</div>
-  )
+    <div className="grid grid-cols-3 gap-2">
+      {tasks.map((task) => (
+        <TaskCard key={task.id} task={task} />
+      ))}
+    </div>
+  );
 }
 
-export default TaskPage
+export default TaskPage;
