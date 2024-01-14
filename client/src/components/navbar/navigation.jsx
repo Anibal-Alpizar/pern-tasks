9a242498a63e3e092f15c9b88154f9c36050f98e
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-13 23:37:33 -0600

    Update navigation icons

--- client/src/components/navbar/navigation.jsx
+++ client/src/components/navbar/navigation.jsx
@@ -1,3 +1,6 @@
+import { MdTaskAlt } from "react-icons/md";
+import { BiTask, BiUserCircle } from "react-icons/bi";
+
 export const publicRoutes = [
   { name: "About", path: "/about" },
   { name: "Login", path: "/login" },
@@ -5,7 +8,7 @@ export const publicRoutes = [
 ];
 
 export const privateRoutes = [
-  { name: "Tasks", path: "/tasks" },
-  { name: "NewTask", path: "/tasks/new" },
-  { name: "Profile", path: "/profile" },
+  { name: "Tasks", path: "/tasks", icon: <BiTask className="w-5 h-5" /> },
+  { name: "Add", path: "/tasks/new", icon: <MdTaskAlt className="w-5 h-5" /> },
+  { name: "Profile", path: "/profile",icon: <BiUserCircle className="w-5 h-5" />, },
 ];
