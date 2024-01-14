2a10d05cb3f73489134a7f7c75e4809ddc47a6b0
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-13 19:48:40 -0600

    Add deleteTaskRequest to tasks.api.js

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -2,3 +2,4 @@ import axios from './axios.js';
 
 export const createTaskRequest = (task) => axios.post('/tasks', task);
 export const getTasksRequest = () => axios.get('/tasks');
+export const deleteTaskRequest = (id) => axios.delete(`/tasks/${id}`);
