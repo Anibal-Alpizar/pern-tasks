150aaecb3e1e9c8332f503a6bc49c15fd07996d0
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-13 22:59:46 -0600

    Add Card component to HomePage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,23 @@
 import { useAuth } from "../context/AuthContext";
+import { Card } from "../components/ui";
 
 function HomePage() {
   const data = useAuth();
   console.log(data);
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Card>
+        <h1 className="text-3xl my-4">Home Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+          aspernatur neque delectus vitae. Modi obcaecati quasi ullam,
+          dignissimos quidem officiis sunt molestiae, illum voluptatum beatae
+          eum maxime nobis hic voluptates omnis ad aliquam distinctio ipsam
+          saepe corrupti. Eum, veritatis assumenda!
+        </p>
+      </Card>
+    </div>
+  );
 }
 
 export default HomePage;
