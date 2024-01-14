0729cda10344ef2a2a5c4ee2741d0e862fdcd9c7
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-13 19:49:13 -0600

    Add delete functionality to TaskCard component

--- client/src/components/tasks/TaskCard.jsx
+++ client/src/components/tasks/TaskCard.jsx
@@ -1,10 +1,24 @@
-import { Card } from "../ui";
+import { Card, Button } from "../ui";
+import { deleteTaskRequest } from "../../api/tasks.api";
 
 function TaskCard({ task }) {
   return (
-    <Card key={task.id}>
-      <h1 className="text-2xl font-bold">{task.title}</h1>
-      <p>{task.description}</p>
+    <Card key={task.id} className="px-7 py-4">
+      <div>
+        <h1 className="text-2xl font-bold">{task.title}</h1>
+        <p>{task.description}</p>
+      </div>
+      <div className="my-2 flex justify-end gap-x-2">
+        <Button>Editar</Button>
+        <Button
+          className="bg-red-500 hover:bg-red-600"
+          onClick={async () => {
+            if (confirm("Are you sure?")) await deleteTaskRequest(task.id);
+          }}
+        >
+          Eliminar
+        </Button>
+      </div>
     </Card>
   );
 }
