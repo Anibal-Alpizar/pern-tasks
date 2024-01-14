263c82a8f0ab04094019b91f82c72d163172e8c7
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-13 23:16:32 -0600

    Refactor ProfilePage component to display user data in a code block

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,7 +2,11 @@ import { useAuth } from "../context/AuthContext";
 
 function ProfilePage() {
   const { user } = useAuth();
-  return <div>{JSON.stringify(user)}</div>;
+  return (
+    <div>
+      <code>{JSON.stringify(user)}</code>
+    </div>
+  );
 }
 
 export default ProfilePage;
