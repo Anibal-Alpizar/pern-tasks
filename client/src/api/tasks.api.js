79d297ccc8c43db14333def4c6739ad6ec4ed18d
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-13 22:22:08 -0600

    Add getTaskRequest to tasks.api.js

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -3,3 +3,4 @@ import axios from './axios.js';
 export const createTaskRequest = (task) => axios.post('/tasks', task);
 export const getTasksRequest = () => axios.get('/tasks');
 export const deleteTaskRequest = (id) => axios.delete(`/tasks/${id}`);
+export const getTaskRequest = (id) => axios.get(`/tasks/${id}`);
