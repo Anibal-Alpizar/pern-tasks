67cb299eab73f1b6ea556b6192e594e1f4b5e17e
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-01-13 22:59:54 -0600

    Update AboutPage component with content

--- client/src/pages/AboutPage.jsx
+++ client/src/pages/AboutPage.jsx
@@ -1,7 +1,16 @@
 function AboutPage() {
   return (
-    <div>AboutPage</div>
-  )
+    <div>
+      <h1 className="text-3xl my-4">About Page</h1>
+      <p className="my-4"> 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam
+        aspernatur neque delectus vitae. Modi obcaecati quasi ullam, dignissimos
+        quidem officiis sunt molestiae, illum voluptatum beatae eum maxime nobis
+        hic voluptates omnis ad aliquam distinctio ipsam saepe corrupti. Eum,
+        veritatis assumenda!
+      </p>
+    </div>
+  );
 }
 
-export default AboutPage
+export default AboutPage;
